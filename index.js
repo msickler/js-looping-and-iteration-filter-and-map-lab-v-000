54d8eef0e01b277fc3601d234c8997f7b0c1d503
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 03:34:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function driverNamesWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) {
     return driver.revenue > revenue
   })
-  return driver.name
+  return drivers.name
 }
